4a89898d4841fc4b7b23f4a9942b3a6e3b269617
locsummer <locnxth06216@gmail.com>
2026-08-16 00:00:55 +0700

    Update CheckoutController.java

--- JewelryShop_SpringBoot/src/main/java/com/agile/jewelryshop/controller/CheckoutController.java
+++ JewelryShop_SpringBoot/src/main/java/com/agile/jewelryshop/controller/CheckoutController.java
@@ -155,7 +155,7 @@ public class CheckoutController {
         return UriComponentsBuilder
                 .fromUriString("https://img.vietqr.io/image/{bankId}-{accountNo}-compact.png")
                 .queryParam("amount", order.getTotalAmount().stripTrailingZeros().toPlainString())
-                .queryParam("addInfo", "LUMIERE " + order.getOrderCode())
+                .queryParam("addInfo", "THANH TOAN")
                 .queryParam("accountName", accountName)
                 .buildAndExpand(bankId, bankAccount)
                 .encode()
